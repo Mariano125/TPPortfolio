ad7e086968b1010f3dbb1b45a255016d4a415ec5
Mariano Duarte <mariano.91.duarte@gmail.com>
2022-07-30 16:55:21 -0300

    Modo noche1

--- js/app.js
+++ js/app.js
@@ -13,10 +13,24 @@ function animateBars(){
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
     nav.classList.toggle('activar');
+    
 }
 
 
+document.querySelector(".moon").addEventListener("click", modonoche, );
 
-nav = document.getElementById('acti')
+let sec1 =document.getElementById("sec1");
+let sec2=document.getElementById("sec2");
+let sec3=document.getElementById("sec3");
+let sec4=document.getElementById("sec4");
+let sec5=document.getElementById("sec5");
 
-
+function modonoche(){
+    
+    sec1.classList.toggle("noche");
+    sec2.classList.toggle("noche");
+    sec3.classList.toggle("noche");
+    sec4.classList.toggle("noche");
+    sec5.classList.toggle("noche");
+    
+}
